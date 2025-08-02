b88ef00185777a1593e5146e4b36bcfb08142560
Anis Jamil <anisjamiloffical1@gmail.com>
2025-08-03 03:30:07 +0500

    more features add

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('customer/<str:pk>/', views.customer, name='customer'),
     path('create_order/<str:pk>/', views.createOrder, name='create_order'),
     path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
-    path('delete_order/<str:pk>/', views.delete_order, name='delete_order')
+    path('delete_order/<str:pk>/', views.delete_order, name='delete_order'),
+    path('register/', views.register_page, name='register'),
+    path('login/', views.login_page, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
+from django.forms import inlineformset_factory
+from .filters import OrderFilter
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register_page(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CreateUserForm()
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+
+def login_page(request):
+   
+        
+    context = {}
+    return render(request, 'accounts/login.html')
+# this function is used to show the home page
 def home(request):
     customer = Customer.objects.first()
     orders = Order.objects.all()
@@ -23,35 +45,44 @@ def home(request):
     }
 
     return render(request, 'accounts/dashboard.html', context=context)
-
+# create a function to show the products
 def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products': products})
-
+# # this function is used to show the customer details and their orders
 def customer(request, pk):
     customer = Customer.objects.get(id=pk)
     orders = customer.order_set.all()
     order_counter = orders.count()
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs  # Apply the filter to the queryset
     context ={
         'customer': customer,
         'orders' : orders,
-        'order_counter': order_counter
+        'order_counter': order_counter,
+        'myFilter': myFilter
     }
     return render(request, 'accounts/customer.html', context=context)
-
+# the commit function for just 1 item in the formset create 
 def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status') ,extra=6)
     customer = Customer.objects.get(id=pk)
-    form = OrderForm()
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer) 
+    # form = OrderForm()
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        # form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
+        # if form.is_valid():
+            # form.save()
+            # return redirect('/')
     else: 
         form = OrderForm(initial={'customer': customer})
-    return render(request, 'accounts/order_form.html', {'form': form})
+    return render(request, 'accounts/order_form.html', {'formset': formset})
 
-
+# this function is used to update the order
 def updateOrder(request, pk):
     order = Order.objects.get(id=pk)
     form = OrderForm(instance=order)
@@ -63,12 +94,9 @@ def updateOrder(request, pk):
     else:
         form = OrderForm(instance=order)
     context = {'form': form}
-
-
-
     return render(request, 'accounts/order_form.html', context)
 
-
+# this function is used to delete the order
 def delete_order(request, pk):
     order = Order.objects.get(id=pk)
     if request.method == 'POST':
